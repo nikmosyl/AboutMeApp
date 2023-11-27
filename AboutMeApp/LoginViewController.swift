3810158e00475f27b163851d9f6d3792e6d3c236
ns.moskalev <nikmosyl@gmail.com>
2023-11-28 02:37:54 +0400

    Complete log in process

--- AboutMeApp/LoginViewController.swift
+++ AboutMeApp/LoginViewController.swift
@@ -9,10 +9,57 @@ import UIKit
 
 class LoginViewController: UIViewController {
 
+    @IBOutlet var userNameTF: UITextField!
+    @IBOutlet var passwordTF: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super .touchesBegan(touches, with: event)
+        view.endEditing(true)
+    }
+    
+    override func shouldPerformSegue(withIdentifier identifier: String, sender: Any?) -> Bool {
+        if userNameTF.text == "1" && passwordTF.text == "1" {
+            return true
+        }
+        showAlert(
+            withTitle: "Invalid login or password",
+            andMessage: "Please, enter correct login and password",
+            andClean: passwordTF
+        )
+        return false
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let welcomeVC = segue.destination as? WelcomeViewController
+        welcomeVC?.name = userNameTF.text
+    }
 
-
+    @IBAction func loginButtonAction() {
+    }
+    
+    @IBAction func forgetUserNameAction() {
+        showAlert(withTitle: "Ooooops!", andMessage: "Your name is 1 😉")
+    }
+    
+    @IBAction func forgetPasswordAction() {
+        showAlert(withTitle: "Ooooops!", andMessage: "Your password is 1 😉")
+    }
+    
+    private func showAlert(
+        withTitle title: String,
+        andMessage message: String,
+        andClean cleaningField: UITextField? = nil
+    ) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "OK", style: .default) { _ in
+                cleaningField?.text = ""
+        }
+        alert.addAction(okAction)
+        present(alert, animated: true)
+    }
 }
 
--- AboutMeApp/WelcomeViewController.swift
+++ AboutMeApp/WelcomeViewController.swift
@@ -7,23 +7,19 @@
 
 import UIKit
 
-class WelcomeViewController: UIViewController {
+final class WelcomeViewController: UIViewController {
 
+    @IBOutlet var welcomeMessageLabel: UILabel!
+    
+    var name: String!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        welcomeMessageLabel.text = "Welcome, \(name ?? "")!"
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func logOutButtonAction() {
+        dismiss(animated: true)
     }
-    */
 
 }
